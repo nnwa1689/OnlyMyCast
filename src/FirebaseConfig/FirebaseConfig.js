ddc1a9e4e0d63de3bf7552c2a062b0f3d35ed02b
Cong-En Wu <nnwa1689@gmail.com>
2021-07-06 14:38:32 +0800

    ignore

---
+++ src/FirebaseConfig/FirebaseConfig.js
@@ -0,0 +1,11 @@
+const FirebaseConfig = {
+  apiKey: "AIzaSyC3LR3I8kQmp6yp8id0Rpjg6781CKj9q1M",
+  authDomain: "onlymycast.firebaseapp.com",
+  databaseURL: "https://onlymycast-default-rtdb.firebaseio.com",
+  projectId: "onlymycast",
+  storageBucket: "onlymycast.appspot.com",
+  messagingSenderId: "432366189476",
+  appId: "1:432366189476:web:f2ef1341e9cf1700205f31",
+  measurementId: "G-4ZLL9J1PQN"
+  };
+export default FirebaseConfig;
